1728ea06061d44c3524120dd91dc064416d277d9
RaselJam <Rasel.jam.h@gmail.com>
2021-01-20 21:05:29 +0100

    Fix Responsiv Layout and sum other Visual Bugs

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function App() {
 						<div class='fanar'></div>
 						<div class='fanar'></div>
 					</div>
-					<ul class='flex my-1'>
+
+					<ul class='menu flex my-1'>
 						<li>
 							<a href='index.html'>Home </a>
 						</li>
@@ -29,13 +30,69 @@ function App() {
 						<h2>Use our Awsome App to free some of your brain memory</h2>
 					</section>
 					<div class='intruduction card'>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt debitis reiciendis ducimus iusto ipsam? Fugit
-						commodi quae dicta totam. Cumque.
+						<h4>We keep everything organaized</h4>
+						<p>
+							Less paper , less time more efficiente working flow.You only need to focus on How and When. We will
+							reminde you your next move acording to your schadule
+						</p>
 					</div>
 				</div>
 			</div>
 			{/* Body */}
-			<section class='main container grid grid-3 bg-thersary p-2'>
+			<section class='main container grid grid-3 bg-thersary'>
+				<div class='tasks card '>
+					<h3 class='with-pen my-1 bg-light'>Tasks to Do</h3>
+					<ul class='tasks-Ul p-1'>
+						<li done='true'>
+							<button class='btn'></button>Paint the car
+						</li>
+						<li done='true'>
+							<button class='btn'>
+								<span class='tooltip'> Delete?</span>
+							</button>
+							Choose the furnitures of Salon
+						</li>
+						<li done='false'>
+							<button class='btn'>
+								<span class='tooltip'>Delete</span>
+							</button>
+							Pay the rent
+						</li>
+						<li>
+							<button class='btn'>
+								<span class='tooltip'>Delete?</span>
+							</button>
+							Pay the rent
+						</li>
+						<li>
+							<button class='btn'>
+								<span class='tooltip'>Delete?</span>
+							</button>
+							Pay the rent
+						</li>
+						<li>
+							{' '}
+							<button class='btn'>
+								<span class='tooltip'>Delete?</span>
+							</button>
+							Pay the rent
+						</li>
+						<li>
+							{' '}
+							<button class='btn'>
+								<span class='tooltip'>Delete?</span>
+							</button>
+							Pay the rent
+						</li>
+					</ul>
+					<div class='adding'>
+						<input type='text' placeholder='Add new Task' />
+						<button class='btn btn-thersary' type='button'>
+							Add
+						</button>
+					</div>
+				</div>
+
 				<div class='tasks card'>
 					<h3 class='with-pen my-1 bg-light'>Tasks to Do</h3>
 					<ul class='tasks-Ul p-1'>
@@ -45,34 +102,14 @@ function App() {
 						<li done>
 							<button class='btn'></button> Choose the furnitures of Salon
 						</li>
-						<li>Pay the rent</li>
 					</ul>
 					<div class='adding'>
-						<input class='my-3' type='text' placeholder='Add new Task' />
-						<button class='btn btn-primary' type='button'>
+						<input type='text' placeholder='Add new Task' />
+						<button class='btn btn-thersary' type='button'>
 							Add
 						</button>
 					</div>
 				</div>
-				<div class='tasks card'>
-					<h3 class='with-pen my-1 bg-light'>Pay the rent</h3>
-					<ul class='tasks-Ul p-1'>
-						<li done='true'>
-							<button class='btn'></button>gather the money
-						</li>
-						<li>
-							<button class='btn'></button> Go to the bank
-						</li>
-						<li>Pay the rent</li>
-					</ul>
-					<div class='adding'>
-						<input class='my-3' type='text' placeholder='Add new sub Task' />
-						<button class='btn btn-primary' type='button'>
-							Add
-						</button>
-					</div>
-				</div>
-				<div class='card'>C</div>
 			</section>
 		</div>
 	)
